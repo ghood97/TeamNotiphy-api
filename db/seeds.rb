5a04a82d4f2c5118f27cd834145764933d5ec55f
Griffin Hood <griffinhood@verizon.net>
2019-11-27 14:12:11 -0500

    Create seed data for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Post.destroy_all
+Post.create(title: 'Post 1', text: 'Post 1 body/text', user_id: 1)
+Post.create(title: 'Post 2', text: 'Post 2 body/text', user_id: 1)
+Post.create(title: 'Post 3', text: 'Post 3 body/text', user_id: 1)
+Post.create(title: 'Post 4', text: 'Post 4 body/text', user_id: 1)
+Post.create(title: 'Post 5', text: 'Post 5 body/text', user_id: 1)
+Post.create(title: 'Post 6', text: 'Post 6 body/text', user_id: 1)
+Post.create(title: 'Post 7', text: 'Post 7 body/text', user_id: 1)
+Post.create(title: 'Post 8', text: 'Post 8 body/text', user_id: 1)
+Post.create(title: 'Post 9', text: 'Post 8 body/text', user_id: 1)
+Post.create(title: 'Post 10', text: 'Post 10 body/text', user_id: 1)
+puts "10 posts created"
