cd931269a7481ec3a5be64e8a8dd7e9bcdb8cd63
Griffin Hood <griffinhood@verizon.net>
2019-12-02 22:32:42 -0500

    Add helper functions in Events serializer to send back formatted date fragments

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,5 +1,26 @@
 # frozen_string_literal: true
 
 class EventSerializer < ActiveModel::Serializer
-  attributes :id, :date, :opponent, :location, :result, :own_score, :opp_score
+  attributes :id,
+             :day,
+             :date,
+             :time,
+             :opponent,
+             :location,
+             :result,
+             :own_score,
+             :opp_score,
+             :user_id
+
+  def day
+    object.date.strftime('%A')
+  end
+
+  def date
+    object.date.strftime('%m/%d/%y')
+  end
+
+  def time
+    object.date.strftime('%I:%M%p')
+  end
 end
