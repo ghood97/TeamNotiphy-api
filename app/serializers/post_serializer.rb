ff9d08a66126395868c9ab2b756a80e8cb058f41
Griffin Hood <griffinhood@verizon.net>
2019-12-02 20:21:22 -0500

    Conditionally serialize :comments
    Created a function to only serailze comments from the Post serializer if
    there is a user signed in

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -3,4 +3,8 @@
 class PostSerializer < ActiveModel::Serializer
   attributes :id, :title, :text, :user, :created_at
   has_many :comments
+
+  def comments
+    return object.comments unless scope.nil?
+  end
 end
