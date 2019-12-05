e93ee5c696b99cbb84dc5ea62794627d3f4d2136
Griffin Hood <griffinhood@verizon.net>
2019-12-05 10:00:37 -0500

    Create user_email helper method
    Change Post to serialize only user email and not user object

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :text, :user, :created_at
+  attributes :id, :title, :text, :user_id, :created_at, :user_email
   has_many :comments
 
   def comments
     return object.comments unless scope.nil?
+  end
+
+  def user_email
+    object.user.email
   end
 end
