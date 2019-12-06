24dc68492d32222dd252f70c7e9dbd2d6a6d0bf3
Griffin Hood <griffinhood@verizon.net>
2019-12-06 00:28:56 -0500

    Update log message after db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,4 +22,5 @@ Post.create(title: 'Post 7', text: 'Post 7 body/text', user_id: 1)
 Post.create(title: 'Post 8', text: 'Post 8 body/text', user_id: 1)
 Post.create(title: 'Post 9', text: 'Post 8 body/text', user_id: 1)
 Post.create(title: 'Post 10', text: 'Post 10 body/text', user_id: 1)
-puts '10 posts created'
+puts '10 Posts created'
+puts 'All Events deleted'
